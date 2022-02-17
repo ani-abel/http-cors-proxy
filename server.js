59c602d5876bc332df81787c6c6d4402c27fa291
Abel Ani <abelanico6@gmail.com>
2022-02-17 12:52:23 +0100

    Added newer url to allow proxy

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var originWhitelist = [
   'http://localhost:8100',
   'http://localhost:4200',
   'http://localhost',
+  'http://cnb.secondcompany.nl',
   'capacitor://localhost',
 ];
 function parseEnvList(env) {
